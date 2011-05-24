b6a12dbf5a6e74c12d543f7323e71464fff076d9
hiro <hiro@mac2hiro.local>
2011-05-24 13:43:16 +0200

    change to mongoid usage

--- lib/uniquify.rb
+++ lib/uniquify.rb
@@ -1,37 +1,40 @@
-module Uniquify
-  def self.included(base)
-    base.extend ClassMethods
-  end
-  
+module Mongoid::Uniquify
+
   def ensure_unique(name)
     begin
       self[name] = yield
-    end while self.class.exists?(name => self[name])
+    end while self.class.where(name => self[name]).exists?
   end
   
   module ClassMethods
-    
-    def uniquify(*args, &block)
-      options = { :length => 8, :chars => ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a }
-      options.merge!(args.pop) if args.last.kind_of? Hash
-      args.each do |name|
-        before_create do |record|
-          if block
-            record.ensure_unique(name, &block)
-          else
+
+    def field_with_uniquify(name, options = {})
+      if options[:uniquify]
+        default = { :length => 8, :chars => ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a, :string => String }
+        options = default.merge!(options)
+
+        field_without_uniquify(name, options)
+                
+        class_eval do
+          validates_uniqueness_of name, :on => :create
+          before_create do |record|
             record.ensure_unique(name) do
               Array.new(options[:length]) { options[:chars].to_a[rand(options[:chars].to_a.size)] }.join
             end
           end
         end
+      else
+        field_without_uniquify(name, options)
       end
     end
-    
   end
-end
 
-if defined?(ActiveRecord::Base)
-  class ActiveRecord::Base
-    include Uniquify
+  def self.included(receiver)
+    receiver.class_eval do
+      extend ClassMethods
+      class << self
+        alias_method_chain :field, :uniquify
+      end
+    end
   end
 end
